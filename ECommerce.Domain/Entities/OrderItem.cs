8b13a9f224b202e8f7c0c025c844a58866c26f98
unknown <elkhanmi@code.edu.az>
2025-03-26 16:24:51 +0400

    fin

--- ECommerce.Domain/Entities/OrderItem.cs
+++ ECommerce.Domain/Entities/OrderItem.cs
@@ -8,7 +8,7 @@
         public int ProductId { get; set; }
         public Product Product { get; set; }
         public int ProductCount { get; set; }
-        public int Quntity { get; set; }
+        public int Quantity { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
 
